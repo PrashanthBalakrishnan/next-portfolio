d13cb93d8a5318fdcd6646b33f8c706209611a0f
Prashanth Balakrishnan <prashbala13@gmail.com>
2023-09-13 19:05:51 -0400

    changed text-sze

--- components/Project.tsx
+++ components/Project.tsx
@@ -37,7 +37,7 @@ export default function Project({
         className="bg-gray-100 max-w-[42rem]  rounded-lg borderBlack overflow-hidden sm:pr-8 relative sm:h-[20rem]  last:mb-0  sm:group-even:pl-8 hover:bg-gray-200 transition dark:bg-white/10 dark:hover:bg-white/20 dark:text-white"
       >
         <div className="pt-4 px-5 sm:pl-10 sm:pr-2 sm:pt-4 sm:max-w-[50%] flex flex-col h-full sm:group-even:ml-[18rem]">
-          <h3 className="text-2xl mt-1 font-semibold">{title}</h3>
+          <h3 className="text-xl mt-1 font-semibold">{title}</h3>
           <p className="my-1 text-sm leading-relaxed text-gray-700 dark:text-white/70">
             {description}
           </p>
