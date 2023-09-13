0a9c3068287b3c26efa682b89f9811b639d60e37
Prashanth Balakrishnan <prashbala13@gmail.com>
2023-09-13 19:15:49 -0400

    fixed card alignments

--- components/Project.tsx
+++ components/Project.tsx
@@ -34,14 +34,14 @@ export default function Project({
     >
       <section
         ref={ref}
-        className="bg-gray-100 max-w-[42rem]  rounded-lg borderBlack overflow-hidden sm:pr-8 relative sm:h-[20rem]  last:mb-0  sm:group-even:pl-8 hover:bg-gray-200 transition dark:bg-white/10 dark:hover:bg-white/20 dark:text-white"
+        className="bg-gray-100 max-w-[42rem]  rounded-lg borderBlack overflow-hidden sm:pr-8 relative sm:h-[22rem]  last:mb-0  sm:group-even:pl-8 hover:bg-gray-200 transition dark:bg-white/10 dark:hover:bg-white/20 dark:text-white"
       >
         <div className="pt-4 px-5 sm:pl-10 sm:pr-2 sm:pt-4 sm:max-w-[50%] flex flex-col h-full sm:group-even:ml-[18rem]">
           <h3 className="text-xl mt-1 font-semibold">{title}</h3>
-          <p className="my-1 text-sm leading-relaxed text-gray-700 dark:text-white/70">
+          <p className=" text-sm leading-relaxed text-gray-700 dark:text-white/70">
             {description}
           </p>
-          <ul className="flex flex-wrap mt-4 gap-2 sm:mt-auto mb-10">
+          <ul className="flex flex-wrap  gap-2 sm:mt-auto mb-10 ">
             {tags.map((tag, idx) => (
               <li
                 className="bg-black/[0.7] px-3 py-1 text-[0.7rem] uppercase tracking-wider text-white rounded-full dark:text-white/70"
@@ -51,7 +51,7 @@ export default function Project({
               </li>
             ))}
           </ul>
-          <div className="flex flex-row items-center justify-center gap-2 mb-2">
+          <div className="mb-5 flex gap-2">
             <a
               className="bg-gray-900 px-3 py-1 text-[0.7rem] uppercase tracking-wider text-white rounded-md dark:text-white/70 hover:bg-black/60 dark:hover:bg-white/40"
               href={gitHub}
