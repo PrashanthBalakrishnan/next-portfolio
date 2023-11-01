7639eb5d5007853a4b6f6b4a95839f807472cd23
Prashanth Balakrishnan <prashbala13@gmail.com>
2023-11-01 08:01:02 -0400

    fixed margins on go back btn

--- components/uiProjects.tsx
+++ components/uiProjects.tsx
@@ -11,7 +11,7 @@ const UiProjects = () => {
   return (
     <section className="flex items-center flex-col px-4 mb-20">
       <Link
-        className="bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition group dark:bg-black/70"
+        className="bg-gray-900 mb-8 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition group dark:bg-black/70"
         href="/"
       >
         Go back
